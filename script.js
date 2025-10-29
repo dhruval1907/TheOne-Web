f20f0ce6c77e875982b8cb3adcc816c7f89c00a4
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-10-29 23:20:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('main'),
+    smooth: true
+});
+
+VanillaTilt.init(document.querySelector(".tilt"), {
+    max: 25,
+    speed: 400,
+});
+
